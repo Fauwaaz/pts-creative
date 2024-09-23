d13e1dc8a7266deb6b8286db5bc2e019491ab1cb
Fauwaaz <fauwaazshaikh999@gmail.com>
2024-09-23 11:13:34 +0530

    fix email global

--- app/components/BlogCard.js
+++ app/components/BlogCard.js
@@ -3,11 +3,13 @@ import Card from "@/app/components/common/Card"
 
 const BlogCard = () => {
   return (
-    <div className="container blog-card grid-2 py">
-          {blogdata.map((item) => (
-              <Card data={item} key={item.id} path="blogs" />
-          ))}
-    </div>
+    <>
+      <div className="container blog-card grid-2 py">
+            {blogdata.map((item) => (
+                <Card data={item} key={item.id} path="blogs" />
+            ))}
+      </div>
+    </>
   )
 }
 
