711d3e334046b7b475feeccb61a8d5452804b414
Fauwaaz <fauwaazshaikh999@gmail.com>
2024-10-15 15:10:31 +0530

    Update: Style change

--- app/components/common/BackgroundLogo.js
+++ app/components/common/BackgroundLogo.js
@@ -32,14 +32,14 @@ const BackgroundLogo = () => {
   return (
     <>
       <motion.div
-        style={{ position: "fixed", top: 0 , zIndex: -999, filter: "brightness(0.5)", opacity: "0.1" }}
+        style={{ position: "fixed", top: 0, right: 0, zIndex: -999, filter: "brightness(0.5)", opacity: "0.1" }}
         animate={controls}
         transition={{ ease: "easeOut", duration: 0.2 }}
       >
         <Image
           src={"/background-logo.webp"}
-          height={705}
-          width={674}
+          height={674}
+          width={705}
           unoptimized={true}
           priority={true}
           alt="Background Image"
